8551921dc5f98218a9dcb8bd3b41ffda4eb1523c
yifengyou <842056007@qq.com>
2025-07-04 21:00:28 +0800

    build success
    Signed-off-by: yifengyou <842056007@qq.com>

--- dc-a588.sh
+++ dc-a588.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-set -x
+set -ex
 
 BS_DIR_TOP=$(cd `dirname $0` ; pwd)
 
-BS_DIR_TOOLCHAIN_ARM64=../prebuilts/gcc/linux-x86/aarch64/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-
+BS_DIR_TOOLCHAIN_ARM64=../gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-
 
-BS_CONFIG_KERNEL=rockchip_linux_defconfig
+# BS_CONFIG_KERNEL=rockchip_linux_defconfig
+BS_CONFIG_KERNEL=dc-a588_defconfig
 BS_CONFIG_KERNEL_DTB=rk3588-evb7-lp4-v10-linux.img
 
 
